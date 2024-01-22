fc9490b7922b7c471c99ff9b389380530932fc4a
Laurent <laurent.roches.simplon@gmail.com>
2024-01-22 16:09:14 +0100

    Lundi 22-01 travail darkmode

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,3 +1,4 @@
+// Faire apparaitre le bouton de retour au top
 let retourTop = document.getElementById ('retourTop');
 
 window.addEventListener ("scroll", boutonRetour);
@@ -8,4 +9,24 @@ function boutonRetour (){
     } else {
         retourTop.classList.replace("visible" , "invisible");
     }
+}
+
+
+//Switch entre Dark et light mode
+let checkboxValue = 'on';
+let checkbox = document.querySelector('.checkbox');
+let bgColor = document.querySelector('.bgColor');
+let bgColorMilieu = document.querySelector('.bgColorMilieu');
+checkbox.addEventListener('click', checkValue);
+
+function checkValue() {
+  if (checkboxValue === 'on') {
+    checkboxValue = 'off';
+    bgColor.classList.replace("lightMode", "darkMode");
+    bgColorMilieu.classList.replace("lightModeMilieu", "darkModeMilieu");
+  } else {
+    checkboxValue = 'on'
+    bgColor.classList.replace("darkMode", "lightMode");
+    bgColorMilieu.classList.replace("darkModeMilieu", "lightModeMilieu");
+  }
 }
