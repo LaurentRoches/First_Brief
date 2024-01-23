5da2132dbbfbaf43f38e6523600375c21a6624fe
Laurent <laurent.roches.simplon@gmail.com>
2024-01-23 16:18:24 +0100

    Menu burger en responsive et animer fait

--- JS/javascript.js
+++ JS/javascript.js
@@ -11,6 +11,29 @@ function boutonRetour (){
     }
 }
 
+//Fermeture du menu burger
+let fermerMenu = () => {
+  let input = document.getElementById('menu-cb');
+  input.checked = false ;
+  let fenetreNode = document.getElementById('menu-cote');
+  fenetreNode.remove();
+}
+let changerEtatMenu = () => {
+  let input = document.getElementById('menu-cb');
+  let actif = input.checked;
+  if (actif){
+    let fenetreNode = document.createElement('div');
+    fenetreNode.id = 'menu-cote';
+    fenetreNode.className ='menu-cote';
+    fenetreNode.addEventListener('click', fermerMenu);
+    document.body.appendChild(fenetreNode);
+  } else {
+    let fenetreNode = document.getElementById('menu-cote');
+    fenetreNode.remove();
+  }
+}
+let input = document.getElementById('menu-cb');
+input.addEventListener('click', changerEtatMenu);
 
 //Switch entre Dark et light mode
 let checkboxValue = 'on';
